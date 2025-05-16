fc876b174790bc5395d448a6120854c4a33a93cb
hemanth3-2004 <nhemanth2004@gmail.com>
2025-05-16 21:17:13 +0530

    final

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -41,7 +41,7 @@ const CropProfitCalculator = () => {
     setArea(100);
     setCrop("Microgreens");
     setUseCustomPrice(false);
-    setCustomPrice(0);
+    setCustomPrice("");
     setProfit(null);
   };
 
